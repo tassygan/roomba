7cb34d0c6effd95b1bce3c1e582c21eb88308394
tassygan <60091345+tassygan@users.noreply.github.com>
2020-01-24 16:55:31 +0600

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,13 +1,21 @@
 import telebot
-from telebot import types
 import random
+import time
+from telebot import types
 from database import SQL
+from search import Search, Tenant
 
 token = "1012837410:AAFY0lxwBFgWPIbRO-lO_MumXnlYJl-1ReQ"
 bot = telebot.TeleBot(token)
 db = SQL()
-name = ""
-age = 0
+mode = 0
+flat_id = 1
+search = Search()
+tenant = Tenant()
+
+search_st = False
+take_st = False
+tenant_st = False
 
 @bot.message_handler(commands = ['start'])
 def start(message):
@@ -26,9 +34,9 @@ def start(message):
 	'- по геолокации #есильский #cарыаркинский #алматинский #байконурский\n')
 
 	keyboard = types.InlineKeyboardMarkup()
-	button1 = types.InlineKeyboardButton('#ищужилье', callback_data = 'search')
-	button2 = types.InlineKeyboardButton('#возьмуксебе', callback_data = 'take')
-	button3 = types.InlineKeyboardButton('#сдамкв', callback_data = 'rent')
+	button1 = types.InlineKeyboardButton('Ищу жилье', callback_data = 'search')
+	button2 = types.InlineKeyboardButton('Возьму к себе', callback_data = 'take')
+	button3 = types.InlineKeyboardButton('Сдам квартиру', callback_data = 'tenant')
 	keyboard.add(button1)
 	keyboard.add(button2)
 	keyboard.add(button3)
@@ -36,18 +44,94 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call:True)
 def callback(call):
+	global mode, search_st, tenant_st, take_st, flat_id
 	if call.message:
 		if call.data == 'search':
 			bot.send_message(call.message.chat.id, 'Введите ваше Имя:')
-			#@bot.message_handler(content_types = ['text'])
-			def name_insert_data(message):
-				name = message.text
-			bot.send_message(call.message.chat.id, 'Введите ваш Возраст:')
-			@bot.message_handler(content_types = ['text'])
-			def age_insert_data(message):
-				age = int(message.text)
-				db.insert_data(name, age)	
+			search_st = True
+			mode = 1
+		elif call.data == 'tenant':
+			bot.send_message(call.message.chat.id, 'Расположение вашей квартиры:')
+			tenant_st = True
+			mode = 1
+		elif call.data == 'flat_out':
+			if flat_id > db.flat_num():
+				bot.send_message(call.message.chat.id, 'Квартиры закончились:(')
+				return
+			flat = db.flat_out(flat_id)
+			flat_id += 1
+			keyboard = types.InlineKeyboardMarkup()
+			button = types.InlineKeyboardButton('Следующая квартира', callback_data = 'flat_out')
+			keyboard.add(button)
+			bot.send_message(call.message.chat.id, flat[1] + '\n' + flat[2] + '\n' + flat[3] + '\n' + flat[4], reply_markup = keyboard)
 
+@bot.message_handler(content_types = ['text'])
+def name_insert_data(message):
+	global search, mode, search_st, tenant_st, take_st
+	if search_st == True:	
+		if mode == 1:
+			search.name = message.text;
+			mode = 2;
+			bot.send_message(message.chat.id, 'Введите ваш возраст:')
+		elif mode == 2: 
+			search.age = int(message.text)
+			mode = 3
+			bot.send_message(message.chat.id, 'Укажите вашу сферу деятельности:')
+		elif mode == 3: 
+			search.sphere = message.text
+			mode = 4
+			bot.send_message(message.chat.id, 'Укажите языки, на которых вы говорите:')
+		elif mode == 4: 
+			search.langs = message.text
+			mode = 5
+			bot.send_message(message.chat.id, 'Ваши интересы, хобби, любимые книги и фильмы')
+		elif mode == 5: 
+			search.interest = message.text
+			mode = 6
+			bot.send_message(message.chat.id, 'Желаемый район города(рядом с..)')
+		elif mode == 6: 
+			search.distr = message.text
+			mode = 7
+			bot.send_message(message.chat.id, 'Желательная цена')
+		elif mode == 7: 
+			search.price = message.text
+			mode = 8
+			bot.send_message(message.chat.id, 'Требования к квартире')
+		elif mode == 8:
+			search.require = message.text
+			mode = 9
+			bot.send_message(message.chat.id, 'Ваш номер телефона:')
+		elif mode == 9:
+			search.phone_num = message.text
+			db.search_insert(search)
+			bot.send_message(message.chat.id, 'Подбираем вам подходящую квартиру...')
+			time.sleep(3)
+			keyboard = types.InlineKeyboardMarkup();
+			button = types.InlineKeyboardButton('Показать квартиру', callback_data = 'flat_out')
+			keyboard.add(button)
+			bot.send_message(message.chat.id, 'Подходящие запросы найдены:', reply_markup = keyboard)
+	elif tenant_st == True:
+		if mode == 1:
+			tenant.location = message.text
+			mode = 2
+			bot.send_message(message.chat.id, 'Цена аренды:')
+		elif mode == 2:
+			tenant.price = message.text
+			mode = 3
+			bot.send_message(message.chat.id, 'Описание вашей квартиры/комнаты (этаж, площадь, \
+			интернет, мебель, бытовая техника)')
+		elif mode == 3:
+			tenant.description = message.text
+			mode = 4
+			bot.send_message(message.chat.id, 'Ваш номер телефона:')
+		elif mode == 4:
+			tenant.phone_num = message.text
+			db.tenant_insert(tenant)
+			bot.send_message(message.chat.id, 'Ваша квартира добавлена!')
+			tenant_st = False
+
+			
+	
 """
 def age_insert_data(message):
 	age = int(message.text)
--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import psycopg2
 
+
+
 class SQL:
 	def __init__(self):
 		self.con = psycopg2.connect(
@@ -10,11 +12,32 @@ class SQL:
 		  port="5432"
 		)
 		self.cur = self.con.cursor()
-	def insert_data(self, name, age):
-		self.cur.execute("INSERT INTO searchers(name, age) VALUES ('%s' , %s)" % (name, age))
+	
+	def search_insert(self, search):
+		self.cur.execute('''
+			INSERT INTO searchers(name, age, sphere, langs, interest, distr, price, require, phone_num) 
+			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+			''', (search.name, search.age, search.sphere, search.langs, search.interest, \
+				search.distr, search.price, search.require, search.phone_num))
 		self.con.commit()
+	
+	def tenant_insert(self, tenant):
+		self.cur.execute('''
+			INSERT INTO tenants(location, price, description, phone_num) 
+			VALUES (%s, %s, %s, %s)
+			''', (tenant.location, tenant.price, tenant.description, tenant.phone_num))
+		self.con.commit()	
+	
 	def close(self):
 		self.con.close()
+	
+	def flat_out(self, idf):
+		self.cur.execute('SELECT * FROM tenants WHERE id = %s', str(idf))
+		return self.cur.fetchone()
+	
+	def flat_num(self):
+		self.cur.execute('SELECT * FROM tenants')
+		return self.cur.rowcount
 '''
 print("Database opened successfully")
 cur = con.cursor()
